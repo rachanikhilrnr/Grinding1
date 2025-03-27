e7fdf4fc0d0a8038ce22255d302f36e04b96d1ed
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2025-03-27 21:34:17 +0530

    eveningCommit

--- server/index.js
+++ server/index.js
@@ -9,5 +9,9 @@ const app = express();
 app.use(express.json());
 
 app.listen(port,() => {
-    console.log("server is running in port number 5000");
+    console.log("server is running in port number $`{port}`");
+})
+
+app.get('',(req,res) => {
+    res.json("Welcome back champ, It's been a very long time!!!!");
 })
